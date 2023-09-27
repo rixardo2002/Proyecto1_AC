20bc1caea7412d6be5cd4fcdfa65f6019460a2ac
ricar <ricardogomezramos@gmail.com>
2023-09-27 16:42:43 +0200

    Primera versión Proyecto

---
+++ src/main/java/es/Proyecto_AC_app.java
@@ -0,0 +1,14 @@
+
+
+package es;
+
+/**
+ *
+ * @author ricar
+ */
+public class Proyecto_AC_app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
