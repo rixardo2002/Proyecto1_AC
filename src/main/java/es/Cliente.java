10800e15ad211fb0b5052b54f1f73969a26cc946
Jaime <jaimedapontealgaba@gmail.com>
2023-09-28 19:50:16 +0200

    Commit con la clase Cliente con sus atributos y constructores

--- src/main/java/es/Cliente.java
+++ src/main/java/es/Cliente.java
@@ -4,6 +4,66 @@ package es;
  *
  * @author Ricardo
  */
+
+
+
+
 public class Cliente {
+    
+    
+    //Atributos de la clase cliente
+    private String nombre;
+    private String numeroTlf;
+    private String ciudad;
+    private int edad;
+
+    //Getter de la clase cliente
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNumeroTlf() {
+        return numeroTlf;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+    //setter de la clase cliente
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNumeroTlf(String numeroTlf) {
+        this.numeroTlf = numeroTlf;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    //Constructores, tanto vacío como completo.
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String numeroTlf, String ciudad, int edad) {
+        this.nombre = nombre;
+        this.numeroTlf = numeroTlf;
+        this.ciudad = ciudad;
+        this.edad = edad;
+    }
+    
+    
+    
     
 }
