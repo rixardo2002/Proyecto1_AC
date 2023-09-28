52f41bcf023c7353389957cd9f7d84f9f1ea5e51
Julian <jbastante369@gmail.com>
2023-09-28 19:52:18 +0200

    Clase concesionario

--- src/main/java/es/Concesionario.java
+++ src/main/java/es/Concesionario.java
@@ -5,5 +5,44 @@ package es;
  * @author Ricardo
  */
 public class Concesionario {
-    // Aqui va la clase de los concesionarios
+    
+    private int id;
+    private String nombre_concesonario;
+    private String localidad;
+
+    public Concesionario(int id, String nombre_concesonario, String localidad) {
+        this.id = id;
+        this.nombre_concesonario = nombre_concesonario;
+        this.localidad = localidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre_concesonario() {
+        return nombre_concesonario;
+    }
+
+    public void setNombre_concesonario(String nombre_concesonario) {
+        this.nombre_concesonario = nombre_concesonario;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Concesionario{" + "id=" + id + ", nombre_concesonario=" + nombre_concesonario + ", localidad=" + localidad + '}';
+    }
+    
 }
