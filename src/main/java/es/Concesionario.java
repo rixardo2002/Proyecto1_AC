1be8ec0d9a66860d999ac1fed93b3b0030cfd2df
Julian <jbastante369@gmail.com>
2023-10-05 19:59:54 +0200

    clase concesionario randomAcess

--- src/main/java/es/Concesionario.java
+++ src/main/java/es/Concesionario.java
@@ -1,19 +1,24 @@
 package es;
 
+import java.util.Arrays;
+
 /**
  *
- * @author Ricardo
+ * @author Julián Bastante
  */
-public class Concesionario {
-    
-    private int id;
-    private String nombre_concesonario;
-    private String localidad;
+final class Concesionario {
 
-    public Concesionario(int id, String nombre_concesonario, String localidad) {
+    public static final int TAMAÑO_REGISTRO = 66;
+    public static final int TAMAÑO_NOMBRE_CONCESIONARIO = 15;
+    public static final int TAMAÑO_LOCALIDAD_CONCESIONARIO = 16;
+    private int id;
+    private char[] nombre_concesonario = new char[Concesionario.TAMAÑO_NOMBRE_CONCESIONARIO];
+    private char[] localidad_concesionario = new char[Concesionario.TAMAÑO_LOCALIDAD_CONCESIONARIO];
+
+    public Concesionario(int id, String nombre_concesonario, String localidad_concesionario) {
         this.id = id;
-        this.nombre_concesonario = nombre_concesonario;
-        this.localidad = localidad;
+        this.setNombre_concesonario(nombre_concesonario);
+        this.setLocalidad_concesionario(localidad_concesionario);
     }
 
     public int getId() {
@@ -24,25 +29,50 @@ public class Concesionario {
         this.id = id;
     }
 
-    public String getNombre_concesonario() {
+    public String getNombre_concesionario() {
+        return new String(this.nombre_concesonario);
+    }
+
+    public char[] getNombre_concesionarioCharArray() {
         return nombre_concesonario;
     }
 
     public void setNombre_concesonario(String nombre_concesonario) {
-        this.nombre_concesonario = nombre_concesonario;
+        this.setNombre_concesionario(nombre_concesonario.toCharArray());
     }
 
-    public String getLocalidad() {
-        return localidad;
+    public void setNombre_concesionario(char[] nombre_concesonario) {
+        Arrays.fill(this.nombre_concesonario, (char) 0);
+        for (int i = 0; i < nombre_concesonario.length && i < this.nombre_concesonario.length; i++) {
+            this.nombre_concesonario[i] = nombre_concesonario[i];
+        }
     }
 
-    public void setLocalidad(String localidad) {
-        this.localidad = localidad;
+    public String getLocalidad_concesionario() {
+        return new String(this.localidad_concesionario);
+    }
+
+    public char[] getLocalidad_concesionarioCharArray() {
+        return localidad_concesionario;
+    }
+
+    public void setLocalidad_concesionario(String localidad_concesionario) {
+        this.setLocalidad_concesionario(localidad_concesionario.toCharArray());
+    }
+
+    public void setLocalidad_concesionario(char[] localidad_concesionario) {
+        Arrays.fill(this.localidad_concesionario, (char) 0);
+        for (int i = 0; i < localidad_concesionario.length && i < this.nombre_concesonario.length; i++) {
+            this.localidad_concesionario[i] = localidad_concesionario[i];
+        }
     }
 
     @Override
     public String toString() {
-        return "Concesionario{" + "id=" + id + ", nombre_concesonario=" + nombre_concesonario + ", localidad=" + localidad + '}';
+        return "Concesionario{"
+                + "id=" + id
+                + ", nombre_concesonario=" + nombre_concesonario
+                + ", localidad=" + localidad_concesionario + '}';
     }
-    
+
 }
