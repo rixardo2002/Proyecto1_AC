516380782857c515c1f941c4519376212b99a53f
Ricar <gmezricar@gmail.com>
2023-10-05 19:47:12 +0200

    he creado la clase concesionario

--- src/main/java/es/MetodosClientes.java
+++ src/main/java/es/MetodosClientes.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package es;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  *
  * @author ricar
  */
 public class MetodosClientes {
+    
+    public static Cliente crearCliente(){
+        Cliente c = new Cliente("Patricia","672456732","Ciudad Real",20);
+        return c; 
+    }
+    
+    public static void clienteAFile() throws IOException {
+        Cliente cli=MetodosClientes.crearCliente();
+        File f = new File(".\\clientes\\"+cli.getNombre()+".txt");//declara fichero
+        if (!f.exists()){
+            f.createNewFile();
+        }
+        FileWriter fw = new FileWriter(f); //crear el flujo de salida
+        fw.write(cli.toString());
+        fw.flush();
+        fw.close();
+    }
+    public static void clienteDesdeFile(){
+        
+    }
     
 }
