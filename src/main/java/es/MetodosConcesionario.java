e1d9becd1e2d1c4f2016d3f32fce9f44c457cdc4
Jaime <jaimedapontealgaba@gmail.com>
2023-10-11 17:12:52 +0200

    Commit en el que en la clase MetodosConcesionario he añadido crear y modificar

--- src/main/java/es/MetodosConcesionario.java
+++ src/main/java/es/MetodosConcesionario.java
@@ -1,12 +1,57 @@
 
 package es;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author Julián
  */
 public class MetodosConcesionario {
-    public static Concesionario crearConcesionario(){
+    Utilidades U;
+    public Concesionario CrearConcesionario() throws IOException{
+
+        Concesionario concesionario=new Concesionario();
+        concesionario.setNombreConcesionario(U.PedirNombreConcesionario());
+        concesionario.setLocalidadConcesionario(U.PedirLocalidadConcesionario());
+        return concesionario;
+             
+    }
+    
+    
+    public Concesionario ModificarConcesionario(Concesionario concesionario) throws IOException{
+        boolean salir=false;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int opc;
+        String leer;
+        do {
+            System.out.println("Que desea modificar??");
+            System.out.println("1.Nombre del concesionario");
+            System.out.println("2.Localidad del concesionario");
+            System.out.println("3.salir");
+            leer=br.readLine();
+            opc=Integer.parseInt(leer);
+            switch (opc) {
+                case 1:
+                     concesionario.setNombreConcesionario(U.PedirNombreConcesionario());
+                    break;
+                case 2:
+                    concesionario.setLocalidadConcesionario(U.PedirLocalidadConcesionario());
+                    break;
+                case 3:
+                    salir=true;
+                 
+                default:
+                    System.out.println("No elegiste la opcion correcta");
+            }
+            
+        } while (salir);
+       
+        
+        
+        return concesionario;
         
     }
 }
