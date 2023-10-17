1bd8d2bbc80826e69fa21b7a36106b1ffb0f40e5
Julian <jbastante369@gmail.com>
2023-10-17 22:02:51 +0200

    Metodos clase cohe

---
+++ src/main/java/es/MetodosCoche.java
@@ -0,0 +1,41 @@
+
+package es;
+
+import java.io.*;
+
+/**
+ *
+ * @author Julián Bastante Serrano
+ */
+public class MetodosCoche {
+     
+    public static void crearCoche(Coches coche) throws IOException{
+        RandomAccessFile file = new RandomAccessFile(new File("coches.dat"), "rw");
+
+        int nuevoID = (int) (file.length() / Coches.TAMANIO_REGISTRO) + 1;
+
+        long posicion = (coche.getId() - 1) * coche.TAMANIO_REGISTRO;
+        file.seek(posicion);
+        file.writeInt(coche.getId());
+        file.write(coche.getMarcaByteArray());
+        file.write(coche.getModeloByteArray());
+
+        file.close();
+    }
+    
+    public static void leerCoche(int id) throws IOException {
+
+        RandomAccessFile file = new RandomAccessFile(new File("coches.dat"), "r");
+
+        //calculo la posición donde comienza el registro
+        long posicion = (id - 1) * Coches.TAMANIO_REGISTRO;
+        file.seek(posicion);
+
+        byte[] arrLeidos = new byte[Coches.TAMANIO_REGISTRO];
+        file.read(arrLeidos);
+        file.close();  //cerrar fichero 
+
+        Coches coche = Coches.createCoche(arrLeidos);
+        System.out.println("Coche: " + coche);
+    }
+}
