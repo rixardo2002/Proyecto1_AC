84404576f1e44588967a18ac54e7c7c5f82f12eb
Julian <jbastante369@gmail.com>
2023-10-28 15:42:19 +0200

    Modificación de metodos coche

--- src/main/java/es/MetodosCoche.java
+++ src/main/java/es/MetodosCoche.java
@@ -8,15 +8,15 @@ import java.io.*;
  * @author Julián Bastante Serrano
  */
 public class MetodosCoche {
-     
+    //el id se genera automaticamente cuando lo escribes el fichero
     public static void crearCoche(Coches coche) throws IOException{
         RandomAccessFile file = new RandomAccessFile(new File("coches.dat"), "rw");
 
         int nuevoID = (int) (file.length() / Coches.TAMANIO_REGISTRO) + 1;
 
-        long posicion = (coche.getId() - 1) * coche.TAMANIO_REGISTRO;
+        long posicion = (nuevoID - 1) * coche.TAMANIO_REGISTRO;
         file.seek(posicion);
-        file.writeInt(coche.getId());
+        file.writeInt(nuevoID);
         file.write(coche.getMarcaByteArray());
         file.write(coche.getModeloByteArray());
 
